48cd2785b2449c57282c237e478a929acb01d48a
C0772144-komalsubhra <komalsubhra6768@gmail.com>
2020-02-07 18:20:38 -0500

    Initial Commit

---
+++ Day3Classses/SStudent.swift
@@ -0,0 +1,22 @@
+//
+//  SStudent.swift
+//  Day3Classses
+//
+//  Created by MacStudent on 2020-02-07.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+struct SStudent{
+    var studentId : Int
+    var studentName : String
+    var totalMarks : Double
+    func display()
+    {
+        print(".....Structure......")
+        print("Student id:\(self.studentId)")
+        print("Student Name:\(self.studentName)")
+        print("total martks:\(self.totalMarks)")
+     
+   }
+}
---
+++ Day3Classses/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Day3Classses
+//
+//  Created by MacStudent on 2020-02-07.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+var a : Int
+var s : String
+
+
+
